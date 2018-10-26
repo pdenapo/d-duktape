d32cb432df03364c5f940f3c693d7defc5323d59
boolangery <eliott.dumeix@gmail.com>
2018-10-26 22:41:55 +0200

    add an example.

---
+++ example/source/app.d
@@ -0,0 +1,58 @@
+/**
+    Duktape exemples.
+*/
+import duktape;
+import std.stdio;
+
+
+enum Direction { up, down, left, right }
+
+static void print(string msg)
+{
+    writeln(msg);
+}
+
+class Person
+{
+private:
+    Direction[] _moveHistory;
+
+public:
+    this() { }
+
+
+    void move(Direction dir)
+    {
+        _moveHistory ~= dir;
+    }
+
+    @property Direction[] moveHistory()
+    {
+        return _moveHistory;
+    }
+}
+
+int main()
+{
+    auto ctx = new DukContext();
+
+    ctx.registerGlobal!print;
+
+    ctx.createNamespace("Game")
+        .register!Direction
+        .register!Person
+        .finalize();
+
+    ctx.evalString(q"{
+        p = new Game.Person();
+
+        p.move(Game.Direction.up);
+        p.move(Game.Direction.down);
+        p.move(Game.Direction.left);
+        p.move(Game.Direction.right);
+
+        print(p.moveHistory().toString());
+    }");
+
+    return 0;
+}
