36c1df66d57275c4010928265818a7566e394348
Pablo De Nápoli <pdenapo@gmail.com>
2020-11-08 19:01:57 -0300

    Begining of adding support for converting a vibe.d Json object to a duktape object.

---
+++ example_json/source/app.d
@@ -0,0 +1,149 @@
+/**
+    Duktape exemples.
+*/
+
+// The modules to use duketape
+import duktape;
+import duk_extras.print_alert;
+import d_duktape;
+
+
+import std.stdio;
+import std.string;
+import std.conv;
+
+import vibe.data.serialization;
+import vibe.data.json;
+
+struct MyStruct
+{
+  int int_data;
+  string string_data;
+  bool boolean_data;
+  // you can mark some fields in your structure as optional
+  // so that they are not requiered to appear in your Json data
+  @optional int optional_data;
+}
+
+enum Direction { up, down, left, right }
+
+static Json get_json()
+{
+  MyStruct other_data= MyStruct(5,"good bye!",true,4);
+  Json other_json = serializeToJson(other_data);
+  return other_json;
+}
+
+/* This function constructs a duktape object based on a vibe.d Json object */
+void push_Json(duk_context *ctx,Json json)
+{
+   switch(json.type())
+   {
+     case Json.Type.int_: duk_push_int(ctx,to!int(json));
+                         break;  
+    case Json.Type.string: duk_push_string(ctx,to!string(json).toStringz());
+                         break;      
+    case Json.Type.bool_: duk_push_boolean(ctx,to!bool(json));
+                         break;
+    case Json.Type.float_: duk_push_number(ctx,to!double(json));
+                         break;
+    case Json.Type.null_: duk_push_null(ctx);
+                         break;
+    case Json.Type.undefined: duk_push_undefined(ctx);
+                    break;                                      
+     default: writeln("Unhandled type in json");
+                assert(0);   
+   } 
+}
+
+extern (C) duk_ret_t int_example(duk_context *ctx) {
+    Json json1 = serializeToJson(5);
+    push_Json(ctx,json1);
+    return 1; // we return something
+}
+
+extern (C) duk_ret_t string_example(duk_context *ctx) {
+    Json json1 = serializeToJson("five");
+    push_Json(ctx,json1);
+    return 1; // we return something
+}
+
+
+extern (C) duk_ret_t boolean_example(duk_context *ctx) {
+    Json json1 = serializeToJson(true);
+    push_Json(ctx,json1);
+    return 1; // we return something
+}
+
+extern (C) duk_ret_t float_example(duk_context *ctx) {
+    Json json1 = serializeToJson(1.2);
+    push_Json(ctx,json1);
+    return 1; // we return something
+}
+
+
+extern (C) duk_ret_t null_example(duk_context *ctx) {
+    Json json1 = serializeToJson(null);
+    push_Json(ctx,json1);
+    return 1; // we return something
+}
+
+
+extern (C) duk_ret_t undefined_example(duk_context *ctx) {
+    Json json1 = Json.undefined();
+    push_Json(ctx,json1);
+    return 1; // we return something
+}
+
+
+void evaluate_js(duk_context *ctx,string line)
+{
+    int ret = duk_peval_string(ctx, line.toStringz());
+
+        if (ret != 0) {
+            writeln("Error: " ~ duk_to_string(ctx, -1).to!string);
+        }
+        else {
+            if (!duk_is_undefined(ctx, -1))
+                writeln(duk_to_string(ctx, -1).to!string);
+        }
+        
+    duk_pop(ctx);
+}
+
+int main()
+{
+
+    duk_context *ctx = duk_create_heap_default();
+    if (!ctx) {
+        writeln("Failed to create a Duktape heap.");
+        return 1;
+    }
+
+    duk_push_global_object(ctx);
+    duk_print_alert_init(ctx, 0);
+    // We register a function in the global object to be used from javascript
+    duk_push_c_function(ctx, &int_example, 1 /*nargs*/);
+    duk_put_prop_string(ctx, -2, "int_example");
+    duk_push_c_function(ctx, &string_example, 1 /*nargs*/);
+    duk_put_prop_string(ctx, -2, "string_example");
+    duk_push_c_function(ctx, &boolean_example, 1 /*nargs*/);
+    duk_put_prop_string(ctx, -2, "boolean_example");
+    duk_push_c_function(ctx, &float_example, 1 /*nargs*/);
+    duk_put_prop_string(ctx, -2, "float_example");
+    duk_push_c_function(ctx, &null_example, 1 /*nargs*/);
+    duk_put_prop_string(ctx, -2, "null_example");
+    duk_push_c_function(ctx, &undefined_example, 1 /*nargs*/);
+    duk_put_prop_string(ctx, -2, "undefined_example");
+
+
+    evaluate_js(ctx,"var object = int_example(); print(object);");
+    evaluate_js(ctx,"var object = string_example(); print(object);");
+    evaluate_js(ctx,"var object = boolean_example(); print(object);");
+    evaluate_js(ctx,"var object = float_example(); print(object);");
+   
+    evaluate_js(ctx,"var object = null_example(); print(object);");
+    evaluate_js(ctx,"var object = undefined_example(); print(object);");
+
+    return 0;
+}
