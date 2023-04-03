8f30a1b93db2ac9ff6e803f825d8d88b717ff58b
Pablo De Nápoli <pdenapo@gmail.com>
2023-04-03 13:40:39 -0300

    Use get instead of to! in conversions. Fixed bug in conversion from Json to string. Updated vibe.d dependency.

--- example_json/source/example_json.d
+++ example_json/source/example_json.d
@@ -6,7 +6,7 @@
 import duktape;
 import duk_extras.print_alert;
 import d_duktape;
-import  d_duktape.json;
+import d_duktape.json;
 
 import std.stdio;
 import std.string;
@@ -14,76 +14,81 @@ import std.conv;
 
 import vibe.data.json;
 
-extern (C) duk_ret_t int_example(duk_context *ctx) {
+extern (C) duk_ret_t int_example(duk_context* ctx)
+{
     Json json1 = serializeToJson(5);
-    push_Json(ctx,json1);
+    push_Json(ctx, json1);
     return 1; // we return something
 }
 
-extern (C) duk_ret_t string_example(duk_context *ctx) {
+extern (C) duk_ret_t string_example(duk_context* ctx)
+{
     Json json1 = serializeToJson("five");
-    push_Json(ctx,json1);
+    push_Json(ctx, json1);
     return 1; // we return something
 }
 
-
-extern (C) duk_ret_t boolean_example(duk_context *ctx) {
+extern (C) duk_ret_t boolean_example(duk_context* ctx)
+{
     Json json1 = serializeToJson(true);
-    push_Json(ctx,json1);
+    push_Json(ctx, json1);
     return 1; // we return something
 }
 
-extern (C) duk_ret_t float_example(duk_context *ctx) {
+extern (C) duk_ret_t float_example(duk_context* ctx)
+{
     Json json1 = serializeToJson(1.2);
-    push_Json(ctx,json1);
+    push_Json(ctx, json1);
     return 1; // we return something
 }
 
-
-extern (C) duk_ret_t null_example(duk_context *ctx) {
+extern (C) duk_ret_t null_example(duk_context* ctx)
+{
     Json json1 = serializeToJson(null);
-    push_Json(ctx,json1);
+    push_Json(ctx, json1);
     return 1; // we return something
 }
 
-
-extern (C) duk_ret_t undefined_example(duk_context *ctx) {
+extern (C) duk_ret_t undefined_example(duk_context* ctx)
+{
     Json json1 = Json.undefined();
-    push_Json(ctx,json1);
+    push_Json(ctx, json1);
     return 1; // we return something
 }
 
-extern (C) duk_ret_t array_example(duk_context *ctx) {
-    Json[3] my_array= [Json(1),Json(2),Json(3)];
+extern (C) duk_ret_t array_example(duk_context* ctx)
+{
+    Json[3] my_array = [Json(1), Json(2), Json(3)];
     Json json1 = Json(my_array);
-    push_Json(ctx,json1);
+    push_Json(ctx, json1);
     return 1; // we return something
 }
 
-
-
-extern (C) duk_ret_t array_example2(duk_context *ctx) {
-    auto my_array1= [[1,2],[2,3]];
+extern (C) duk_ret_t array_example2(duk_context* ctx)
+{
+    auto my_array1 = [[1, 2], [2, 3]];
     Json json1 = serializeToJson(my_array1);
-    push_Json(ctx,json1);
+    push_Json(ctx, json1);
     return 1; // we return something
 }
 
-
-extern (C) duk_ret_t associative_array_example(duk_context *ctx) {
+extern (C) duk_ret_t associative_array_example(duk_context* ctx)
+{
     int[string] dayNumbers =
-    [ "Monday": 0, "Tuesday" : 1, "Wednesday" : 2,
-    "Thursday" : 3, "Friday" : 4, "Saturday" : 5,
-    "Sunday": 6 ];
+        [
+            "Monday": 0, "Tuesday": 1, "Wednesday": 2,
+            "Thursday": 3, "Friday": 4, "Saturday": 5,
+            "Sunday": 6
+        ];
     Json json1 = serializeToJson(dayNumbers);
-    push_Json(ctx,json1);
+    push_Json(ctx, json1);
     return 1; // we return something
 }
 
-string evaluate_js(duk_context *ctx,string line)
+string evaluate_js(duk_context* ctx, string line)
 {
     int ret = duk_peval_string(ctx, line.toStringz());
-    string  result = duk_to_string(ctx, -1).to!string;
+    string result = duk_to_string(ctx, -1).to!string;
     // writeln(result);
     duk_pop(ctx);
     return result;
@@ -92,8 +97,9 @@ string evaluate_js(duk_context *ctx,string line)
 int main()
 {
 
-    duk_context *ctx = duk_create_heap_default();
-    if (!ctx) {
+    duk_context* ctx = duk_create_heap_default();
+    if (!ctx)
+    {
         writeln("Failed to create a Duktape heap.");
         return 1;
     }
@@ -101,53 +107,54 @@ int main()
     duk_push_global_object(ctx);
     duk_print_alert_init(ctx, 0);
     // We register a function in the global object to be used from javascript
-    duk_push_c_function(ctx, &int_example, 1 /*nargs*/);
+    duk_push_c_function(ctx, &int_example, 1 /*nargs*/ );
     duk_put_prop_string(ctx, -2, "int_example");
-    duk_push_c_function(ctx, &string_example, 1 /*nargs*/);
+    duk_push_c_function(ctx, &string_example, 1 /*nargs*/ );
     duk_put_prop_string(ctx, -2, "string_example");
-    duk_push_c_function(ctx, &boolean_example, 1 /*nargs*/);
+    duk_push_c_function(ctx, &boolean_example, 1 /*nargs*/ );
     duk_put_prop_string(ctx, -2, "boolean_example");
-    duk_push_c_function(ctx, &float_example, 1 /*nargs*/);
+    duk_push_c_function(ctx, &float_example, 1 /*nargs*/ );
     duk_put_prop_string(ctx, -2, "float_example");
-    duk_push_c_function(ctx, &null_example, 1 /*nargs*/);
+    duk_push_c_function(ctx, &null_example, 1 /*nargs*/ );
     duk_put_prop_string(ctx, -2, "null_example");
-    duk_push_c_function(ctx, &undefined_example, 1 /*nargs*/);
+    duk_push_c_function(ctx, &undefined_example, 1 /*nargs*/ );
     duk_put_prop_string(ctx, -2, "undefined_example");
-    duk_push_c_function(ctx, &array_example, 1 /*nargs*/);
+    duk_push_c_function(ctx, &array_example, 1 /*nargs*/ );
     duk_put_prop_string(ctx, -2, "array_example");
-    duk_push_c_function(ctx, &array_example2, 1 /*nargs*/);
+    duk_push_c_function(ctx, &array_example2, 1 /*nargs*/ );
     duk_put_prop_string(ctx, -2, "array_example2");
-    duk_push_c_function(ctx, &associative_array_example, 1 /*nargs*/);
+    duk_push_c_function(ctx, &associative_array_example, 1 /*nargs*/ );
     duk_put_prop_string(ctx, -2, "associative_array_example");
-   
-    const string days_in_json ="{\"Sunday\":6,\"Thursday\":3,\"Wednesday\":2,\"Saturday\":5,\"Tuesday\":1,\"Monday\":0,\"Friday\":4}";
+
+    const string days_in_json = "{\"Sunday\":6,\"Thursday\":3,\"Wednesday\":2,\"Saturday\":5,\"Tuesday\":1,\"Monday\":0,\"Friday\":4}";
 
     // print(object) would give [object Object], something opaque 
     // It is better to use print(JSON.stringify(object)) when debugging javascript code!
- 
-     auto test_cases = [ ["int_example()","5"],
-                      ["string_example()","\"five\""],
-                      ["boolean_example()","true"],
-                      ["float_example()","1.2"],
-                      ["null_example()","null"],
-                      ["undefined_example()","undefined"],
-                      ["JSON.stringify(array_example())","[1,2,3]"],
-                      ["JSON.stringify(array_example2())","[[1,2],[2,3]]"],
-                      ["JSON.stringify(associative_array_example())",days_in_json],
-                      ["var object = associative_array_example(); object[\"Saturday\"]","5"]
-                    ];
 
-    bool all_passed=true;
-    foreach (c;test_cases)
+    auto test_cases = [
+        ["int_example()", "5"],
+        ["string_example()", "five"],
+        ["boolean_example()", "true"],
+        ["float_example()", "1.2"],
+        ["null_example()", "null"],
+        ["undefined_example()", "undefined"],
+        ["JSON.stringify(array_example())", "[1,2,3]"],
+        ["JSON.stringify(array_example2())", "[[1,2],[2,3]]"],
+        ["JSON.stringify(associative_array_example())", days_in_json],
+        ["var object = associative_array_example(); object[\"Saturday\"]", "5"]
+    ];
+
+    bool all_passed = true;
+    foreach (c; test_cases)
     {
-      string result = evaluate_js(ctx,c[0]);
-      bool test_passed = result==c[1];
-      write(result,"\t");
-      if (test_passed)
+        string result = evaluate_js(ctx, c[0]);
+        bool test_passed = result == c[1];
+        write(result, "\t");
+        if (test_passed)
             writeln("TEST PASSED");
-      else 
+        else
             writeln("TEST FAILED");
-      all_passed = all_passed && test_passed;    
+        all_passed = all_passed && test_passed;
     }
 
     if (all_passed)
--- source/d_duktape/json.d
+++ source/d_duktape/json.d
@@ -5,82 +5,95 @@ import std.stdio;
 import std.string;
 
 import duktape;
+import core.stdc.stdio : printf;
 import d_duktape.json;
 
 //import vibe.data.serialization;
 import vibe.data.json;
 
-
 /* This function constructs a duktape object based on a vibe.d Json object */
-void push_Json(duk_context *ctx,Json json)
+void push_Json(duk_context* ctx, Json json)
 {
-   switch(json.type())
-   {
-     case Json.Type.int_: duk_push_int(ctx,to!int(json));
-                         break;  
-    case Json.Type.string: duk_push_string(ctx,to!string(json).toStringz());
-                         break;      
-    case Json.Type.bool_: duk_push_boolean(ctx,to!bool(json));
-                         break;
-    case Json.Type.float_: duk_push_number(ctx,to!double(json));
-                         break;
-    case Json.Type.null_: duk_push_null(ctx);
-                         break;
-    case Json.Type.undefined: duk_push_undefined(ctx);
-                    break;
-    case Json.Type.array: duk_idx_t arr_idx = duk_push_array(ctx);
-                        int i=0;
-                        foreach (value;json.byValue())
-                        {
-                          push_Json(ctx,value);
-                          duk_put_prop_index(ctx, arr_idx, i);
-                          i++;  
-                        };               
-                        break;
-    case Json.Type.object: duk_idx_t obj_idx = duk_push_object(ctx);
-                        foreach (key,value;json.byKeyValue)
-                        {
-                          push_Json(ctx,value);
-                          duk_put_prop_string(ctx, obj_idx,key.toStringz());
-                        };               
-                        break;
-   // the case bigInt is not supperted 
-   default: writeln("Unhandled type in push_Json");
-                assert(0);   
-   } 
+  //debug writeln("push_Json al comenzar");
+  switch (json.type())
+  {
+  case Json.Type.int_:
+    duk_push_int(ctx, json.get!int);
+    break;
+  case Json.Type.string:
+    // No usar to!string para la conversión, que agrega comillas
+    immutable(char)* s = json.get!string.toStringz();
+    duk_push_string(ctx, s);
+    break;
+  case Json.Type.bool_:
+    duk_push_boolean(ctx, json.get!bool);
+    break;
+  case Json.Type.float_:
+    duk_push_number(ctx, json.get!double);
+    break;
+  case Json.Type.null_:
+    duk_push_null(ctx);
+    break;
+  case Json.Type.undefined:
+    duk_push_undefined(ctx);
+    break;
+  case Json.Type.array:
+    duk_idx_t arr_idx = duk_push_array(ctx);
+    int i = 0;
+    foreach (value; json.byValue())
+    {
+      push_Json(ctx, value);
+      duk_put_prop_index(ctx, arr_idx, i);
+      i++;
+    };
+    break;
+  case Json.Type.object:
+    duk_idx_t obj_idx = duk_push_object(ctx);
+    foreach (key, value; json.byKeyValue)
+    {
+      push_Json(ctx, value);
+      duk_put_prop_string(ctx, obj_idx, key.toStringz());
+    };
+    break;
+    // the case bigInt is not supperted 
+  default:
+    writeln("Unhandled type in push_Json");
+    assert(0);
+  }
 }
 
-Json duk_to_Json(duk_context *ctx,  duk_idx_t idx)
+Json duk_to_Json(duk_context* ctx, duk_idx_t idx)
 {
+  debug writeln("duk_to_Json al comenzar");
   Json result;
   duk_int_t which_type = duk_get_type(ctx, idx);
   switch (which_type)
   {
-    case DUK_TYPE_NULL:
-          result=Json(null);
-          break;
+  case DUK_TYPE_NULL:
+    result = Json(null);
+    break;
 
-    case DUK_TYPE_BOOLEAN: 
-          result=Json(duk_get_boolean(ctx, idx));
-          break;   
+  case DUK_TYPE_BOOLEAN:
+    result = Json(duk_get_boolean(ctx, idx));
+    break;
 
-    case DUK_TYPE_NUMBER: 
-          result=Json(duk_get_number(ctx, idx));
-          break;
+  case DUK_TYPE_NUMBER:
+    result = Json(duk_get_number(ctx, idx));
+    break;
 
-    case DUK_TYPE_STRING:
-          auto s=fromStringz(duk_get_string(ctx, idx));
-          result= Json(to!string(s));
-          break;
+  case DUK_TYPE_STRING:
+    auto s = fromStringz(duk_get_string(ctx, idx));
+    result = Json(to!string(s));
+    break;
 
-   case DUK_TYPE_OBJECT:
-        writeln("Es un objeto");
-        break;
+  case DUK_TYPE_OBJECT:
+    writeln("Es un objeto");
+    break;
 
-
-   // the case bigInt is not supperted 
-   default: writeln("Unhandled type in duk_to_Json ",which_type);
-                assert(0);       
+    // the case bigInt is not supperted 
+  default:
+    writeln("Unhandled type in duk_to_Json ", which_type);
+    assert(0);
   }
   return result;
 }
